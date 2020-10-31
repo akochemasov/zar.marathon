3d076973629ff44d6873b47ea1bc5436df921dc1
akochemasov <alexander.kochemasov@litota.ru>
2020-11-01 00:30:43 +0400

    Generic array and method map

--- src/example.ts
+++ src/example.ts
@@ -16,3 +16,14 @@ const MyHometask: HomeTaskInterface = {
     simeArray: ["string one", "string two", 42],
     withData: [{ howIDoIt: "I Do It Wel", simeArray: ["string one", 23] }],
 }
+
+//Generic
+interface MyArray<T> {
+    [N: number]: T;
+
+    map<U>(fn: (el: T) => U): U[];
+}
+
+const tsArr: MyArray<number> = [1, 2, 3, 4, 5];
+tsArr.map((i) => i + 1);
+tsArr.map((i) => `${i} + 1`);
