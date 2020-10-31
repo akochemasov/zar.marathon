89ed0e5296ade246fd68d182479d5b96968f5037
akochemasov <alexander.kochemasov@litota.ru>
2020-10-31 23:58:49 +0400

    Add interface

--- src/example.ts
+++ src/example.ts
@@ -1,2 +1,18 @@
 //Объединение 2х строк
 const concat = (str1:string, str2:string):string => `${str1} ${str2}`;
+
+//Интерфейс
+interface FieldsInteface {
+    howIDoIt: string,
+    simeArray: any[]
+}
+
+interface HomeTaskInterface extends FieldsInteface {
+    withData: FieldsInteface[]
+}
+
+const MyHometask: HomeTaskInterface = {
+    howIDoIt: "I Do It Wel",
+    simeArray: ["string one", "string two", 42],
+    withData: [{ howIDoIt: "I Do It Wel", simeArray: ["string one", 23] }],
+}
