cdafbffc98f437f1cd95eb5fee69d0852801f8b7
akochemasov <alexander.kochemasov@litota.ru>
2020-11-04 11:58:32 +0400

    Remove ts-ignore, because fix webpack config for css-modules

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,7 @@ import cn from 'classnames';
 import style from './App.modules.scss';
 
 const App = () => {
-  return (
-    // @ts-ignore
-    <div className={cn(style.header, ' color')}>This is App Component</div>
-  );
+  return <div className={cn(style.header, ' color')}>This is App Component</div>;
 };
 
 export default App;
