e3a6b057780379c51236d76cd2405575b32e0dbc
akochemasov <alexander.kochemasov@litota.ru>
2020-11-04 12:01:57 +0400

    Clear App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
 import React from 'react';
-import cn from 'classnames';
 
 import style from './App.modules.scss';
 
 const App = () => {
-  return <div className={cn(style.header, ' color')}>This is App Component</div>;
+  return <div className={style.app}>App</div>;
 };
 
 export default App;
