de0c87e593daea5d15baf494ad2596368edc98e0
akochemasov <alexander.kochemasov@litota.ru>
2020-11-06 22:05:31 +0400

    Clear App

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,16 +1,7 @@
 import React from 'react';
-import Header from '../Header';
-import Footer from '../Footer';
-
-import style from './App.module.scss';
 
 const App = () => {
-  return (
-    <div className={style.app}>
-      <Header />
-      <Footer />
-    </div>
-  );
+  return <></>;
 };
 
 export default App;
