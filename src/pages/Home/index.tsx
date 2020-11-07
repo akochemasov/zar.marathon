ea6333456e7518f5658d913fbd70a1ffa1801c58
akochemasov <alexander.kochemasov@litota.ru>
2020-11-07 16:23:07 +0400

    Rename folder common

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Header from '../../components/Header';
-import Button from '../../components/common/Button';
+import Button from '../../components/Common/Button';
 
 import style from './Home.module.scss';
 
