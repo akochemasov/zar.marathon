85dfe7de803e0f261cc644dedd606c67f96f460d
akochemasov <alexander.kochemasov@litota.ru>
2020-11-07 22:43:10 +0400

    Create and add Layout component

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from '../../components/Header';
 import Button from '../../components/Common/Button';
 import Parallax from '../../components/Parrallax';
+import Layout from '../../components/Layout';
 
 import style from './Home.module.scss';
 
@@ -9,13 +10,20 @@ const Home = () => {
   return (
     <div className={style.root}>
       <Header />
-      <Button
-        onClick={() => {
-          alert('click');
-        }}>
-        See pokemons
-      </Button>
-      <Parallax />
+      <Layout>
+        <div>
+          <p>You can know the type of Pokemon, its strengths, disadvantages and abilities</p>
+          <Button
+            onClick={() => {
+              alert('click');
+            }}>
+            See pokemons
+          </Button>
+        </div>
+      </Layout>
+      <div>
+        <Parallax />
+      </div>
     </div>
   );
 };
