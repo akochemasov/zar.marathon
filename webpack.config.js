7ff1c55c8d8689edbbb2264672ffa25e6539524b
akochemasov <alexander.kochemasov@litota.ru>
2020-11-07 16:20:35 +0400

    Config for images other types

--- webpack.config.js
+++ webpack.config.js
@@ -61,6 +61,10 @@ module.exports = {
             },
           },
         ],
+      },
+      {
+        test: [/\.bmp$/, /\.gif$/, /\.jpe?g$/, /\.png$/],
+        use: ['url-loader'],
       }
     ],
   },
