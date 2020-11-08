37e1113fcda7c4947fa38206b1f21c8237489006
akochemasov <alexander.kochemasov@litota.ru>
2020-11-08 14:45:24 +0400

    Add routing

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
 import HomePage from '../../pages/Home';
 import Pokedex from '../../pages/Pokedex';
 
 const App = () => {
   return (
-    <>
-      <HomePage />
-      {false && <Pokedex />}
-    </>
+    <BrowserRouter>
+      <Route path="/" exact component={HomePage} />
+      <Route path="/home" component={HomePage} />
+      <Route path="/pokedex" component={Pokedex} />
+      <Route render={() => <h1>404</h1>} />
+    </BrowserRouter>
   );
 };
 
