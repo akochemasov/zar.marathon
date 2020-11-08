e88c75ec2d9d78ea5198fa86cdb97b7dd7cc7731
akochemasov <alexander.kochemasov@litota.ru>
2020-11-08 13:24:13 +0400

    Other types Button

--- src/components/Common/Button/index.tsx
+++ src/components/Common/Button/index.tsx
@@ -1,14 +1,20 @@
 import React from 'react';
+import cn from 'classnames';
 
 import style from './Button.module.scss';
 
+type Sizes = 'big' | 'small' | 'width';
+type Colors = 'green' | 'yellow';
+
 interface ButtonProps {
   onClick: (event: React.MouseEvent<HTMLButtonElement>) => void;
+  size: Sizes;
+  color: Colors;
 }
 
-const Button: React.FC<ButtonProps> = ({ children, onClick }) => {
+const Button: React.FC<ButtonProps> = ({ children, onClick, size = 'big', color = 'green' }) => {
   return (
-    <button type="button" className={style.root} onClick={onClick}>
+    <button type="button" className={cn(style.root, style[size], style[color])} onClick={onClick}>
       {children}
     </button>
   );
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -17,7 +17,9 @@ const Home = () => {
             <b>Find</b> all your favorite <b>Pokemon</b>
           </Heading>
           <p>You can know the type of Pokemon, its strengths, disadvantages and abilities</p>
-          <Button onClick={() => {}}>See pokemons</Button>
+          <Button onClick={() => {}} size="big" color="green">
+            See pokemons
+          </Button>
         </div>
         <div className={style.contentParallax}>
           <Parallax />
