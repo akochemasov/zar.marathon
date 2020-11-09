c7a8aaebf0c9e3cfff70e27e7b994ffdd0be07b4
akochemasov <alexander.kochemasov@litota.ru>
2020-11-09 23:24:16 +0400

    Object Menu move in routes.tsx

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,11 +1,35 @@
 import React from 'react';
 import Home from './pages/Home';
 import Pokedex from './pages/Pokedex';
+import Empty from './pages/Empty';
 
-const routes = {
-  '/': () => <Home />,
-  '/home': () => <Home />,
-  '/pokedex': () => <Pokedex />,
-};
+const MENU = [
+  {
+    title: 'Home',
+    link: '/',
+    component: () => <Home />,
+  },
+  {
+    title: 'Pokédex',
+    link: '/pokedex',
+    component: () => <Pokedex />,
+  },
+  {
+    title: 'Legendaries',
+    link: '/legendaries',
+    component: () => <Empty title="Legendaries" />,
+  },
+  {
+    title: 'Documentation',
+    link: '/documentation',
+    component: () => <Empty title="Documentation" />,
+  },
+];
+
+const routes = MENU.reduce((acc, item) => {
+  // @ts-ignore
+  acc[item.link] = item.component;
+  return acc;
+}, {});
 
 export default routes;
