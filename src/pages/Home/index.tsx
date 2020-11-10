0a483c73967ea607e89af296e3426dc4ad0e992f
akochemasov <alexander.kochemasov@litota.ru>
2020-11-10 12:45:43 +0400

    Change props Heading

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -14,7 +14,7 @@ const HomePage = () => {
       <Header />
       <Layout className={style.contentWrap}>
         <div className={style.contentText}>
-          <Heading type="h1">
+          <Heading size="h1">
             <b>Find</b> all your favorite <b>Pokemon</b>
           </Heading>
           <p>You can know the type of Pokemon, its strengths, disadvantages and abilities</p>
